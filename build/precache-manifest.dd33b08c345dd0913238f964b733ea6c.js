c2635fce135e08a1b0f8946ad422cb926d37e0db
rishgupta2301 <guptarishabh1122@gmail.com>
2022-06-09 20:18:27 +0530

    first commmit

---
+++ build/precache-manifest.dd33b08c345dd0913238f964b733ea6c.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c9e7621cabae2aba468495df48f3f2a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b04e1e419a99623a9c0",
+    "url": "/static/css/main.a2e7bd60.chunk.css"
+  },
+  {
+    "revision": "623d2a99ddffa95676e9",
+    "url": "/static/js/2.4667406a.chunk.js"
+  },
+  {
+    "revision": "d8341c57f580e145d476bfdbdb6453b4",
+    "url": "/static/js/2.4667406a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b04e1e419a99623a9c0",
+    "url": "/static/js/main.710b7923.chunk.js"
+  },
+  {
+    "revision": "ef1c30d77b4a6036e3ce",
+    "url": "/static/js/runtime-main.84d226e5.js"
+  },
+  {
+    "revision": "971e3181ae6735636e2c49cca0418f61",
+    "url": "/static/media/John Denver - Country Roads.971e3181.jpg"
+  },
+  {
+    "revision": "a9f11dd7dc47db0768a140b89d89de83",
+    "url": "/static/media/John Denver - Country Roads.a9f11dd7.mp3"
+  },
+  {
+    "revision": "2640f833069973cd0823c1f709b6353a",
+    "url": "/static/media/Khalid - Young Dumb Broke.2640f833.jpg"
+  },
+  {
+    "revision": "e92d0309fbda5f2e2a063e0cc5834124",
+    "url": "/static/media/Khalid - Young Dumb Broke.e92d0309.mp3"
+  },
+  {
+    "revision": "a085f2f0164d605a717084c32baf2274",
+    "url": "/static/media/Never Gonna Give You Up.a085f2f0.png"
+  },
+  {
+    "revision": "4977e46cbdad85bad906db660d0a25a3",
+    "url": "/static/media/Post Malone - White Iverson.4977e46c.mp3"
+  },
+  {
+    "revision": "68d6ebbb90f987df3665106e55131836",
+    "url": "/static/media/Post Malone - White Iverson.68d6ebbb.png"
+  },
+  {
+    "revision": "ce3d38a985854c4640fe89ab293679ac",
+    "url": "/static/media/Rick Astley - Never Gonna Give You Up.ce3d38a9.mp3"
+  },
+  {
+    "revision": "70ab2f27726c9ce5f3eeb1b54787a478",
+    "url": "/static/media/Sigrid - High Five.70ab2f27.mp3"
+  },
+  {
+    "revision": "92a83443ea257110bbbedbaa7ac80d50",
+    "url": "/static/media/Sigrid - High Five.92a83443.png"
+  },
+  {
+    "revision": "0f702e6d9615af4ea6e4503fa3754645",
+    "url": "/static/media/game.0f702e6d.jpg"
+  },
+  {
+    "revision": "86f652825909378dd28f81eb87ed4dd3",
+    "url": "/static/media/music.86f65282.jpg"
+  },
+  {
+    "revision": "626bd78a090cd747f7bfe1addd07babd",
+    "url": "/static/media/settings.626bd78a.png"
+  },
+  {
+    "revision": "227f0ba9833ea10638eb851b4937d9f3",
+    "url": "/static/media/wallpaper1.227f0ba9.jpg"
+  },
+  {
+    "revision": "23077a93618a97f5d7254432926d3d69",
+    "url": "/static/media/wallpaper2.23077a93.jpg"
+  },
+  {
+    "revision": "b2fda5cab36fb65eccf326028ec5e1bb",
+    "url": "/static/media/wallpaper3.b2fda5ca.jpg"
+  }
+]);
